f15451ff24f258abff67f1379c45c0cec9b65dc2
felixvicent <felixvicent1306@gmail.com>
2022-10-12 14:12:36 -0300

    create home component

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,10 @@
+import * as S from "./styles";
+
+export function Main() {
+  return (
+    <S.Wrapper>
+      <S.Title>Boilerplate</S.Title>
+      <S.Description>A template from a project using NextJs</S.Description>
+    </S.Wrapper>
+  );
+}
